4a05696f67e6a74962df7fdfd5ee35bdba797e11
axelmaddonni <axel.maddonni@gmail.com>
2019-05-26 20:03:15 -0300

    Import fixes

--- src/image_segmentation_cnn/data_loader.py
+++ src/image_segmentation_cnn/data_loader.py
@@ -3,9 +3,11 @@ import cv2
 import glob
 import itertools
 import os
-from tqdm import tqdm
 import random
 import pprint
+import pandas as pd
+import csv
+from keras.utils import to_categorical
 
 IMAGE_ORDERING = 'channels_first'
 # from ..models.config import IMAGE_ORDERING
@@ -13,7 +15,7 @@ IMAGE_ORDERING = 'channels_first'
 
 random.seed(0)
 
-def get_image_arr( path , width , height , imgNorm="sub_mean" , odering='channels_first' ):
+def get_image_arr( path , width , height , imgNorm="divide" , odering='channels_first' ):
 	if type( path ) is np.ndarray:
 		img = path
 	else:
@@ -36,57 +38,57 @@ def get_image_arr( path , width , height , imgNorm="sub_mean" , odering='channel
 		img = np.rollaxis(img, 2, 0)
 	return img
 
-    def read_annotations(path):
-        with open(path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            annotations = []
-            for row in csv_reader:
-                annotations.append(row[1:])
-            return np.array(annotations)
+def read_annotations(path):
+	with open(path) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		annotations = []
+		for row in csv_reader:
+			annotations.append(row[1:])
+		return np.array(annotations)
 
-    def read_annotations_positions(path):
-        with open(path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            positions = []
-            for row in csv_reader:
-                positions.append(row[1:])
-            return np.array(positions)
+def read_annotations_positions(path):
+	with open(path) as csv_file:
+		csv_reader = csv.reader(csv_file, delimiter=',')
+		positions = []
+		for row in csv_reader:
+			positions.append(row[1:])
+		return np.array(positions)
 
-    def get_segmentation_arr(image_number, height, width, annotations, positions):
-        int_labels = np.zeros(width * height)
-        label_number = 0
-        for [row, col] in positions:
-            int_labels[int(row)*int(col)] = annotations[image_number][label_number]
-            label_number+= 1
-        return to_categorical(int_labels, num_classes=6)
+def get_segmentation_arr(image_number, height, width, annotations, positions):
+	int_labels = np.zeros(width * height)
+	label_number = 0
+	for [row, col] in positions:
+		int_labels[int(row)*int(col)] = annotations[image_number][label_number]
+		label_number+= 1
+	return to_categorical(int_labels, num_classes=6)
 
-    def image_segmentation_generator(images_path, annotations_path, positions_path, batch_size, input_height, input_width, do_augment=False ):
-        images = glob.glob( os.path.join(images_path,"*.jpg")  ) + glob.glob( os.path.join(images_path,"*.JPG")  )
+def image_segmentation_generator(images_path, annotations_path, positions_path, batch_size, input_height, input_width, do_augment=False ):
+	images = glob.glob( os.path.join(images_path,"*.jpg")  ) + glob.glob( os.path.join(images_path,"*.JPG")  )
 
-        image_numbers = list(range(50))
-        random.shuffle( image_numbers )
-        zipped = itertools.cycle( image_numbers  )
+	image_numbers = list(range(50))
+	random.shuffle( image_numbers )
+	zipped = itertools.cycle( image_numbers  )
 
-        annotations = read_annotations(annotations_path)
-        positions = read_annotations_positions(positions_path)
+	annotations = read_annotations(annotations_path)
+	positions = read_annotations_positions(positions_path)
 
-        while True:
-            X = []
-            Y = []
-            for _ in range(batch_size) :
-                image_number = next(zipped)
-                img = cv2.imread(images[image_number], cv2.IMREAD_COLOR)
+	while True:
+		X = []
+		Y = []
+		for _ in range(batch_size) :
+			image_number = next(zipped)
+			img = cv2.imread(images[image_number], cv2.IMREAD_COLOR)
 
-                # if do_augment:
-                    # img , seg[:,:,0] = augment_seg( img , seg[:,:,0] )
+			# if do_augment:
+				# img , seg[:,:,0] = augment_seg( img , seg[:,:,0] )
 
-                X.append(get_image_arr(img , input_width , input_height ,odering=IMAGE_ORDERING))
-                Y.append(get_segmentation_arr(image_number, input_height, input_width, annotations, positions))
+			X.append(get_image_arr(img , input_width , input_height ,odering=IMAGE_ORDERING))
+			Y.append(get_segmentation_arr(image_number, input_height, input_width, annotations, positions))
 
-            pprint.pprint(f"Image shape: {np.array(X).shape} ") # (batch_size, channels, height, width)
-            pprint.pprint(f"Annotation shape: {np.array(Y).shape}") # (batch_size, height x width, n_classes)
-            yield np.array(X) , np.array(Y)
+		pprint.pprint(f"Image shape: {np.array(X).shape} ") # (batch_size, channels, height, width)
+		pprint.pprint(f"Annotation shape: {np.array(Y).shape}") # (batch_size, height x width, n_classes)
+		yield np.array(X) , np.array(Y)
 
 # Used to create labels_trasposed.csv
 def traspose_csv():
-    pd.read_csv('../data/raw/labels.csv', header=None).T.to_csv('../data/raw/labels_traposed.csv', header=False, index=False)
+	pd.read_csv('../data/raw/labels.csv', header=None).T.to_csv('../data/raw/labels_traposed.csv', header=False, index=False)
--- src/main.py
+++ src/main.py
@@ -1,22 +1,39 @@
 from image_segmentation_cnn.model import unet
 
+from image_segmentation_cnn.data_loader import image_segmentation_generator
+import pprint
+
 def main():
-    model = unet(n_classes=51 ,  input_height=416, input_width=608  )
 
-    model.train(
-        train_images =  "../data/raw/images",
-        train_annotations = "../data/raw/annotations",
-        epochs=5
-    )
+    input_height = 1555
+    input_width = 1555
+    n_classes = 6
+    # Class 0 represents UNKNOWN
 
-    out = model.predict_segmentation(
-        inp="../data/raw/images",
-        out_fname="out.png"
-    )
+    images_path = '../data/raw/images'
+    annotations_path = '../data/raw/labels_trasposed.csv'
+    positions_path = '../data/raw/rowCol.csv'
 
-    import matplotlib.pyplot as plt
-    plt.imshow(out)
+    generator = image_segmentation_generator(images_path, annotations_path, positions_path, 2, input_height, input_width)
+    [img, seg] = next(generator)
+    print(img.shape)
+    print(seg.shape)
 
+    # model = unet(n_classes=51 ,  input_height=416, input_width=608  )
+
+    # model.train(
+    #     train_images =  "../data/raw/images_prepped_train",
+    #     train_annotations = "../data/raw/annotations_prepped_train",
+    #     epochs=5
+    # )
+
+    # out = model.predict_segmentation(
+    #     inp="../data/raw/images",
+    #     out_fname="out.png"
+    # )
+
+    # import matplotlib.pyplot as plt
+    # plt.imshow(out)
 
 if __name__ == '__main__':
     main()

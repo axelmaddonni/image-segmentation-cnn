5b2eae62785c21642fadd412fad8e17ed4ddef55
axelmaddonni <axel.maddonni@gmail.com>
2019-05-26 19:56:35 -0300

    Updates data loader functions with our data sources, refactor model

--- src/image_segmentation_cnn/data_loader.py
+++ src/image_segmentation_cnn/data_loader.py
@@ -5,30 +5,13 @@ import itertools
 import os
 from tqdm import tqdm
 import random
+import pprint
 
 IMAGE_ORDERING = 'channels_first'
 # from ..models.config import IMAGE_ORDERING
 # from augmentation import augment_seg
 
 random.seed(0)
-class_colors = [  ( random.randint(0,255),random.randint(0,255),random.randint(0,255)   ) for _ in range(5000)  ]
-
-def get_pairs_from_paths( images_path , segs_path ):
-	images = glob.glob( os.path.join(images_path,"*.jpg")  ) + glob.glob( os.path.join(images_path,"*.png")  ) +  glob.glob( os.path.join(images_path,"*.jpeg")  )
-	segmentations  =  glob.glob( os.path.join(segs_path,"*.png")  )
-
-	segmentations_d = dict( zip(segmentations,segmentations ))
-
-	ret = []
-
-	for im in images:
-		seg_bnme = os.path.basename(im).replace(".jpg" , ".png").replace(".jpeg" , ".png")
-		seg = os.path.join( segs_path , seg_bnme  )
-		assert ( seg in segmentations_d ),  (im + " is present in "+images_path +" but "+seg_bnme+" is not found in "+segs_path + " . Make sure annotation image are in .png"  )
-		ret.append((im , seg) )
-
-	return ret
-
 
 def get_image_arr( path , width , height , imgNorm="sub_mean" , odering='channels_first' ):
 	if type( path ) is np.ndarray:
@@ -53,61 +36,57 @@ def get_image_arr( path , width , height , imgNorm="sub_mean" , odering='channel
 		img = np.rollaxis(img, 2, 0)
 	return img
 
+    def read_annotations(path):
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            annotations = []
+            for row in csv_reader:
+                annotations.append(row[1:])
+            return np.array(annotations)
 
-def get_segmentation_arr( path , nClasses ,  width , height , no_reshape=False ):
-	seg_labels = np.zeros((  height , width  , nClasses ))
+    def read_annotations_positions(path):
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            positions = []
+            for row in csv_reader:
+                positions.append(row[1:])
+            return np.array(positions)
 
-	if type( path ) is np.ndarray:
-		img = path
-	else:
-		img = cv2.imread(path, 1)
+    def get_segmentation_arr(image_number, height, width, annotations, positions):
+        int_labels = np.zeros(width * height)
+        label_number = 0
+        for [row, col] in positions:
+            int_labels[int(row)*int(col)] = annotations[image_number][label_number]
+            label_number+= 1
+        return to_categorical(int_labels, num_classes=6)
 
-	img = cv2.resize(img, ( width , height ) , interpolation=cv2.INTER_NEAREST )
-	img = img[:, : , 0]
+    def image_segmentation_generator(images_path, annotations_path, positions_path, batch_size, input_height, input_width, do_augment=False ):
+        images = glob.glob( os.path.join(images_path,"*.jpg")  ) + glob.glob( os.path.join(images_path,"*.JPG")  )
 
-	for c in range(nClasses):
-		seg_labels[: , : , c ] = (img == c ).astype(int)
+        image_numbers = list(range(50))
+        random.shuffle( image_numbers )
+        zipped = itertools.cycle( image_numbers  )
 
-	if no_reshape:
-		return seg_labels
+        annotations = read_annotations(annotations_path)
+        positions = read_annotations_positions(positions_path)
 
-	seg_labels = np.reshape(seg_labels, ( width*height , nClasses ))
-	return seg_labels
+        while True:
+            X = []
+            Y = []
+            for _ in range(batch_size) :
+                image_number = next(zipped)
+                img = cv2.imread(images[image_number], cv2.IMREAD_COLOR)
 
+                # if do_augment:
+                    # img , seg[:,:,0] = augment_seg( img , seg[:,:,0] )
 
-def verify_segmentation_dataset( images_path , segs_path , n_classes ):
-	img_seg_pairs = get_pairs_from_paths( images_path , segs_path )
+                X.append(get_image_arr(img , input_width , input_height ,odering=IMAGE_ORDERING))
+                Y.append(get_segmentation_arr(image_number, input_height, input_width, annotations, positions))
 
-	assert len(img_seg_pairs)>0 , "Dataset looks empty or path is wrong "
+            pprint.pprint(f"Image shape: {np.array(X).shape} ") # (batch_size, channels, height, width)
+            pprint.pprint(f"Annotation shape: {np.array(Y).shape}") # (batch_size, height x width, n_classes)
+            yield np.array(X) , np.array(Y)
 
-	for im_fn , seg_fn in tqdm(img_seg_pairs) :
-		img = cv2.imread( im_fn )
-		seg = cv2.imread( seg_fn )
-
-		assert ( img.shape[0]==seg.shape[0] and img.shape[1]==seg.shape[1] ) , "The size of image and the annotation does not match or they are corrupt "+ im_fn + " " + seg_fn
-		assert ( np.max(seg[:,:,0]) < n_classes) , "The pixel values of seg image should be from 0 to "+str(n_classes-1) + " . Found pixel value "+str(np.max(seg[:,:,0]))
-
-	print("Dataset verified! ")
-
-
-def image_segmentation_generator( images_path , segs_path ,  batch_size,  n_classes , input_height , input_width , output_height , output_width  , do_augment=False ):
-	img_seg_pairs = get_pairs_from_paths( images_path , segs_path )
-	random.shuffle( img_seg_pairs )
-	zipped = itertools.cycle( img_seg_pairs  )
-
-	while True:
-		X = []
-		Y = []
-		for _ in range( batch_size) :
-			im , seg = next(zipped)
-
-			im = cv2.imread(im , 1 )
-			seg = cv2.imread(seg , 1 )
-
-			# if do_augment:
-				# img , seg[:,:,0] = augment_seg( img , seg[:,:,0] )
-
-			X.append( get_image_arr(im , input_width , input_height ,odering=IMAGE_ORDERING )  )
-			Y.append( get_segmentation_arr( seg , n_classes , output_width , output_height )  )
-
-		yield np.array(X) , np.array(Y)
+# Used to create labels_trasposed.csv
+def traspose_csv():
+    pd.read_csv('../data/raw/labels.csv', header=None).T.to_csv('../data/raw/labels_traposed.csv', header=False, index=False)
--- src/image_segmentation_cnn/model.py
+++ src/image_segmentation_cnn/model.py
@@ -24,65 +24,57 @@ def vanilla_encoder( input_height ,  input_width  ):
 	x = img_input
 	levels = []
 
-	x = (ZeroPadding2D((pad,pad) , data_format=IMAGE_ORDERING ))( x )
-	x = (Conv2D(filter_size, (kernel, kernel) , data_format=IMAGE_ORDERING , padding='valid'))( x )
-	x = (BatchNormalization())( x )
-	x = (Activation('relu'))( x )
-	x = (MaxPooling2D((pool_size, pool_size) , data_format=IMAGE_ORDERING  ))( x )
-	levels.append( x )
-
-	x = (ZeroPadding2D((pad,pad) , data_format=IMAGE_ORDERING ))( x )
-	x = (Conv2D(128, (kernel, kernel) , data_format=IMAGE_ORDERING , padding='valid'))( x )
-	x = (BatchNormalization())( x )
-	x = (Activation('relu'))( x )
-	x = (MaxPooling2D((pool_size, pool_size) , data_format=IMAGE_ORDERING  ))( x )
-	levels.append( x )
-
-
-	for _ in range(3):
-		x = (ZeroPadding2D((pad,pad) , data_format=IMAGE_ORDERING ))(x)
-		x = (Conv2D(256, (kernel, kernel) , data_format=IMAGE_ORDERING , padding='valid'))(x)
-		x = (BatchNormalization())(x)
-		x = (Activation('relu'))(x)
-		x = (MaxPooling2D((pool_size, pool_size) , data_format=IMAGE_ORDERING  ))(x)
+	for _ in range(4):
+		x = (ZeroPadding2D((pad,pad) , data_format=IMAGE_ORDERING ))( x )
+		x = (Conv2D(filter_size, (kernel, kernel) , data_format=IMAGE_ORDERING , padding='valid'))( x )
+		x = (BatchNormalization())( x )
+		x = (Activation('relu'))( x )
+		x = (MaxPooling2D((pool_size, pool_size) , data_format=IMAGE_ORDERING  ))( x )
 		levels.append( x )
+		filter_size = filter_size * 2
 
 	return img_input , levels
 
 
 def unet( n_classes=4 , l1_skip_conn=True,  input_height=416, input_width=608  ):
 
+	kernel = 3
+	filter_size = 512
+	pad = 1
+	pool_size = 2
+
 	img_input , levels = vanilla_encoder( input_height=input_height ,  input_width=input_width )
-	[f1 , f2 , f3 , f4 , f5 ] = levels
+	[f1 , f2 , f3 , f4] = levels
 
 	o = f4
 
-	o = ( ZeroPadding2D( (1,1) , data_format=IMAGE_ORDERING ))(o)
-	o = ( Conv2D(512, (3, 3), padding='valid', data_format=IMAGE_ORDERING))(o)
+	o = ( ZeroPadding2D( (pad, pad) , data_format=IMAGE_ORDERING ))(o)
+	o = ( Conv2D(512, (kernel, kernel), padding='valid', data_format=IMAGE_ORDERING))(o)
 	o = ( BatchNormalization())(o)
-
-	o = ( UpSampling2D( (2,2), data_format=IMAGE_ORDERING))(o)
+	o = ( UpSampling2D( (pool_size, pool_size), data_format=IMAGE_ORDERING))(o)
 	o = ( concatenate([ o ,f3],axis=MERGE_AXIS )  )
-	o = ( ZeroPadding2D( (1,1), data_format=IMAGE_ORDERING))(o)
-	o = ( Conv2D( 256, (3, 3), padding='valid', data_format=IMAGE_ORDERING))(o)
-	o = ( BatchNormalization())(o)
 
-	o = ( UpSampling2D( (2,2), data_format=IMAGE_ORDERING))(o)
+	o = ( ZeroPadding2D( (pad, pad), data_format=IMAGE_ORDERING))(o)
+	o = ( Conv2D( 256, (kernel, kernel), padding='valid', data_format=IMAGE_ORDERING))(o)
+	o = ( BatchNormalization())(o)
+	o = ( UpSampling2D( (pool_size, pool_size), data_format=IMAGE_ORDERING))(o)
 	o = ( concatenate([o,f2],axis=MERGE_AXIS ) )
-	o = ( ZeroPadding2D((1,1) , data_format=IMAGE_ORDERING ))(o)
-	o = ( Conv2D( 128 , (3, 3), padding='valid' , data_format=IMAGE_ORDERING ) )(o)
+
+	o = ( ZeroPadding2D((pad, pad) , data_format=IMAGE_ORDERING ))(o)
+	o = ( Conv2D( 128, (kernel, kernel), padding='valid', data_format=IMAGE_ORDERING))(o)
 	o = ( BatchNormalization())(o)
-
-	o = ( UpSampling2D( (2,2), data_format=IMAGE_ORDERING))(o)
-
-	if l1_skip_conn:
+	o = ( UpSampling2D( (pool_size, pool_size), data_format=IMAGE_ORDERING))(o)
+	if l1_skip_conn: # what is this for ?
 		o = ( concatenate([o,f1],axis=MERGE_AXIS ) )
 
-	o = ( ZeroPadding2D((1,1)  , data_format=IMAGE_ORDERING ))(o)
-	o = ( Conv2D( 64 , (3, 3), padding='valid'  , data_format=IMAGE_ORDERING ))(o)
+	o = ( ZeroPadding2D((pad, pad)  , data_format=IMAGE_ORDERING ))(o)
+	o = ( Conv2D( 64, (kernel, kernel), padding='valid', data_format=IMAGE_ORDERING))(o)
 	o = ( BatchNormalization())(o)
+	### add extra up sampling
+	o = ( UpSampling2D( (pool_size, pool_size), data_format=IMAGE_ORDERING))(o)
+	###
 
-	o =  Conv2D( n_classes , (3, 3) , padding='same', data_format=IMAGE_ORDERING )( o )
+	o =  Conv2D( n_classes , (kernel, kernel) , padding='same', data_format=IMAGE_ORDERING)(o)
 
 	model = get_segmentation_model(img_input , o )
 
